2e4f159be886ddc0327d0e55add60fc0ae56e6f0
hborcher <hborcher@illinois.edu>
2017-02-23 16:27:48 -0600

    refactored checkers into own file

--- dcc_qc/validators.py
+++ dcc_qc/validators.py
@@ -1,9 +1,6 @@
 import abc
-import os
-from collections import namedtuple
-import re
 
-Results = namedtuple("Results", ["valid", "errors"])
+from dcc_qc import checkers
 
 
 class AbsFactory(metaclass=abc.ABCMeta):
@@ -28,46 +25,40 @@ class AbsFactory(metaclass=abc.ABCMeta):
         pass
 
 
-class AbsValidator(metaclass=abc.ABCMeta):
-    @abc.abstractmethod
-    def check(self, file_):
-        pass
-
-
 class AccessValidators(AbsFactory):
     @staticmethod
     def metadata_checker():
-        return AccessMetadataChecker
+        return checkers.AccessMetadataChecker
 
     @staticmethod
     def technical_checker():
-        return AccessTechnicalChecker
+        return checkers.AccessTechnicalChecker
 
     @staticmethod
     def completeness_checker():
-        return AccessCompletenessChecker
+        return checkers.AccessCompletenessChecker
 
     @staticmethod
     def naming_checker():
-        return AccessNamingChecker()
+        return checkers.AccessNamingChecker()
 
 
 class PreservationValidators(AbsFactory):
     @staticmethod
     def metadata_checker():
-        return PresMetadataChecker()
+        return checkers.PresMetadataChecker()
 
     @staticmethod
     def technical_checker():
-        return PresTechnicalChecker()
+        return checkers.PresTechnicalChecker()
 
     @staticmethod
     def completeness_checker():
-        return PresCompletenessChecker()
+        return checkers.PresCompletenessChecker()
 
     @staticmethod
     def naming_checker():
-        return PresNamingChecker()
+        return checkers.PresNamingChecker()
 
 
 # =================
@@ -75,101 +66,3 @@ class PreservationValidators(AbsFactory):
 # =================
 
 
-class PresCompletenessChecker(AbsValidator):
-    # TODO: Create PresCompletenessChecker() class
-    def check(self, file):
-        pass
-
-
-class PresNamingChecker(AbsValidator):
-    valid_extensions = [".tif"]
-    valid_naming_scheme = re.compile("^\d{8}$")
-
-    def check(self, file):
-        valid = True
-        errors = []
-
-        basename, extension = os.path.splitext(os.path.basename(file))
-
-        if extension not in PresNamingChecker.valid_extensions:
-            valid = False
-            errors.append("Invalid preservation file extension: \"{}\"".format(extension))
-
-        # Check the image files have the full 8 digits
-        if extension == ".tif":
-            if "target" not in basename:
-                if PresNamingChecker.valid_naming_scheme.match(basename) is None:
-                    valid = False
-                    errors.append(
-                        "\"{}\" does not match the valid file name pattern for preservation files".format(basename))
-
-        return Results(valid=valid, errors=errors)
-
-
-class PresMetadataChecker(AbsValidator):
-    # TODO: Create PresMetadataChecker() class
-    def check(self, file):
-        pass
-
-
-class PresTechnicalChecker(AbsValidator):
-    # TODO: Create PresTechnicalChecker() class
-    def check(self, file):
-        pass
-
-
-class AccessCompletenessChecker(AbsValidator):
-    # TODO: Create AccessCompletenessChecker() class
-    def check(self, file):
-        pass
-
-
-class AccessNamingChecker(AbsValidator):
-    # TODO: Create AccessNamingChecker() class
-    valid_extensions = [".tif", ".txt", ".md5", ".xml", ".yml"]
-    valid_naming_scheme = re.compile("^\d{8}$")
-
-    def check(self, file):
-        valid = True
-        errors = []
-
-        basename, extension = os.path.splitext(os.path.basename(file))
-
-        if extension not in AccessNamingChecker.valid_extensions:
-            valid = False
-            errors.append("Invalid file access file extension: \"{}\"".format(extension))
-
-        # Check the image files have the full 8 digits
-        if extension == ".tif" or extension == ".txt":
-            if AccessNamingChecker.valid_naming_scheme.match(basename) is None:
-                valid = False
-                errors.append(
-                    "\"{}\" does not match the valid file name pattern for preservation files".format(basename))
-
-        # The only xml file should be marc.xml
-        if extension == ".xml":
-            if basename != "marc":
-                valid = False
-                errors.append(
-                    "\"{}\" does not match the valid file name pattern for preservation files".format(basename))
-
-        # The only yaml file should be meta.yml
-        if extension == ".yml":
-            if basename != "meta":
-                valid = False
-                errors.append(
-                    "\"{}\" does not match the valid file name pattern for preservation files".format(basename))
-
-        return Results(valid=valid, errors=errors)
-
-
-class AccessMetadataChecker(AbsValidator):
-    # TODO: Create AccessMetadataChecker() class
-    def check(self, file):
-        pass
-
-
-class AccessTechnicalChecker(AbsValidator):
-    # TODO: Create AccessTechnicalChecker() class
-    def check(self, file):
-        pass
