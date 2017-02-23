34858f7a800d409de59702fbf2b5bbf241497692
hborcher <hborcher@illinois.edu>
2017-02-23 14:13:22 -0600

    fixture names are not the same as their functions

--- tests/test_bad_files.py
+++ tests/test_bad_files.py
@@ -9,8 +9,8 @@ BASE_ROOT = "T:\\"
 # Fixtures
 # =======================
 
-@pytest.fixture
-def access_bad_7209692():
+@pytest.fixture(name="access_7209692")
+def files_access_bad_7209692():
     """
     Note:
         Images 00000004, 00000008, have incorrect Job Identifier metadata.
@@ -30,8 +30,8 @@ def access_bad_7209692():
     return os.walk(os.path.normpath(os.path.join(BASE_ROOT, r"HenryTest-PSR_2\DCC\Access_BAD\7209692")))
 
 
-@pytest.fixture
-def access_bad_7210012():
+@pytest.fixture(name="access_7210012")
+def files_access_bad_7210012():
     """
     Note:
         Images 00000007, 00000016, 00000027, 00000033 have incorrect specs for access files.
@@ -43,8 +43,8 @@ def access_bad_7210012():
     return os.walk(os.path.normpath(os.path.join(BASE_ROOT, r"HenryTest-PSR_2\DCC\Access_BAD\7210012")))
 
 
-@pytest.fixture
-def access_bad_7210438():
+@pytest.fixture(name="access_7210438")
+def files_access_bad_7210438():
     """
     Note:
         All images have incorrect specs for access files.
@@ -57,8 +57,8 @@ def access_bad_7210438():
     return os.walk(os.path.normpath(os.path.join(BASE_ROOT, r"HenryTest-PSR_2\DCC\Access_BAD\7210438")))
 
 
-@pytest.fixture
-def access_good():
+@pytest.fixture(name="access_good")
+def files_access_good():
     """
     Note:
         contains 7208771 and 7209666
@@ -72,8 +72,8 @@ def access_good():
     return os.walk(os.path.normpath(os.path.join(BASE_ROOT, r"HenryTest-PSR_2\DCC\Access_GOOD")))
 
 
-@pytest.fixture
-def preservation_bad_7208772():
+@pytest.fixture(name="preservation_7208772")
+def files_preservation_bad_7208772():
     """
     Note:
         All images have incorrect specs for preservation files.
@@ -85,8 +85,8 @@ def preservation_bad_7208772():
     return os.walk(os.path.normpath(os.path.join(BASE_ROOT, r"HenryTest-PSR_2\DCC\Preservation_BAD\7208772")))
 
 
-@pytest.fixture
-def preservation_bad_7209934():
+@pytest.fixture(name="preservation_7209934")
+def files_preservation_bad_7209934():
     """
     Note:
         Images 00000005, 00000010, 00000015, 00000020, 00000025 have incorrect specs for preservation files.
@@ -97,8 +97,8 @@ def preservation_bad_7209934():
     return os.walk(os.path.normpath(os.path.join(BASE_ROOT, r"HenryTest-PSR_2\DCC\Preservation_BAD\7209934")))
 
 
-@pytest.fixture
-def preservation_good():
+@pytest.fixture(name="preservation_good")
+def files_preservation_good():
     """
     Note:
         Contains 6895567 & 7210439
@@ -115,13 +115,13 @@ def preservation_good():
 # Tests
 # =======================
 
-def test_access_job_identifier_bad(access_bad_7209692):
+def test_access_job_identifier_bad(access_7209692):
     invalid_files = ["00000004.tif", '00000008.tif']
 
     validator_factory = validators.AccessValidators()
     validator = validator_factory.metadata_checker()
 
-    for root, dirs, files in access_bad_7209692:
+    for root, dirs, files in access_7209692:
         for file_ in files:
 
             file_name = os.path.join(root, file_)
@@ -132,13 +132,13 @@ def test_access_job_identifier_bad(access_bad_7209692):
                 assert result.valid is True
 
 
-def test_access_metadata_title_incorrect(access_bad_7209692):
+def test_access_metadata_title_incorrect(access_7209692):
     invalid_files = ["00000014.tif", '00000022.tif']
 
     validator_factory = validators.AccessValidators()
     validator = validator_factory.metadata_checker()
 
-    for root, dirs, files in access_bad_7209692:
+    for root, dirs, files in access_7209692:
         for file_ in files:
 
             file_name = os.path.join(root, file_)
@@ -149,13 +149,13 @@ def test_access_metadata_title_incorrect(access_bad_7209692):
                 assert result.valid is True
 
 
-def test_access_metadata_credit_line_missing(access_bad_7209692):
+def test_access_metadata_credit_line_missing(access_7209692):
     invalid_files = ["00000023.tif"]
 
     validator_factory = validators.AccessValidators()
     validator = validator_factory.metadata_checker()
 
-    for root, dirs, files in access_bad_7209692:
+    for root, dirs, files in access_7209692:
         for file_ in files:
 
             file_name = os.path.join(root, file_)
@@ -166,13 +166,13 @@ def test_access_metadata_credit_line_missing(access_bad_7209692):
                 assert result.valid is True
 
 
-def test_access_metadata_creator_missing(access_bad_7209692):
+def test_access_metadata_creator_missing(access_7209692):
     invalid_files = ["00000026.tif"]
 
     validator_factory = validators.AccessValidators()
     validator = validator_factory.metadata_checker()
 
-    for root, dirs, files in access_bad_7209692:
+    for root, dirs, files in access_7209692:
         for file_ in files:
 
             file_name = os.path.join(root, file_)
@@ -183,7 +183,7 @@ def test_access_metadata_creator_missing(access_bad_7209692):
                 assert result.valid is True
 
 
-def test_access_specs_incorrect(access_bad_7210012):
+def test_access_specs_incorrect(access_7210012):
     invalid_files = ["00000007.tif",
                      "00000016.tif",
                      "00000027.tif",
@@ -192,7 +192,7 @@ def test_access_specs_incorrect(access_bad_7210012):
     validator_factory = validators.AccessValidators()
     validator = validator_factory.technical_checker()
 
-    for root, dirs, files in access_bad_7210012:
+    for root, dirs, files in access_7210012:
         for file_ in files:
 
             file_name = os.path.join(root, file_)
@@ -204,7 +204,6 @@ def test_access_specs_incorrect(access_bad_7210012):
 
 
 def test_access_specs_correct(access_good):
-
     validator_factory = validators.AccessValidators()
     validator = validator_factory.technical_checker()
 
@@ -214,13 +213,12 @@ def test_access_specs_correct(access_good):
             assert validator.check(file_name) is True
 
 
-def test_access_incorrect_file_naming(access_bad_7210012):
-
+def test_access_incorrect_file_naming(access_7210012):
     invalid_files = ["0000020.tif"]
     validator_factory = validators.AccessValidators()
-    validator = validator_factory.check_namming()
+    validator = validator_factory.naming_checker()
 
-    for root, dirs, files in access_bad_7210012:
+    for root, dirs, files in access_7210012:
         for file_ in files:
 
             file_name = os.path.join(root, file_)
@@ -232,7 +230,6 @@ def test_access_incorrect_file_naming(access_bad_7210012):
 
 
 def test_access_files_found_all(access_good):
-
     validator_factory = validators.AccessValidators()
     validator = validator_factory.completeness_checker()
 
@@ -240,59 +237,53 @@ def test_access_files_found_all(access_good):
     assert not result.valid
 
 
-def test_access_files_text_missing(access_bad_7210438):
-
+def test_access_files_text_missing(access_7210438):
     validator_factory = validators.AccessValidators()
     validator = validator_factory.completeness_checker()
 
-    result = validator.check(access_bad_7210438)
+    result = validator.check(access_7210438)
     assert not result.valid
 
 
-def test_access_files_checksum_missing(access_bad_7210438):
-
+def test_access_files_checksum_missing(access_7210438):
     validator_factory = validators.AccessValidators()
     validator = validator_factory.completeness_checker()
 
-    result = validator.check(access_bad_7210438)
+    result = validator.check(access_7210438)
     assert not result.valid
 
 
-def test_access_files_marc_missing(access_bad_7210438):
-
+def test_access_files_marc_missing(access_7210438):
     validator_factory = validators.AccessValidators()
     validator = validator_factory.completeness_checker()
 
-    result = validator.check(access_bad_7210438)
+    result = validator.check(access_7210438)
     assert not result.valid
 
 
-def test_access_files_meta_missing(access_bad_7210438):
-
+def test_access_files_meta_missing(access_7210438):
     validator_factory = validators.AccessValidators()
     validator = validator_factory.completeness_checker()
 
-    result = validator.check(access_bad_7210438)
+    result = validator.check(access_7210438)
     assert not result.valid
 
 
-def test_preservation_files_targets_missing(preservation_bad_7208772):
-
+def test_preservation_files_targets_missing(preservation_7208772):
     validator_factory = validators.PreservationValidators()
     validator = validator_factory.completeness_checker()
-    
-    result = validator.check(preservation_bad_7208772) 
+
+    result = validator.check(preservation_7208772)
     assert not result.valid
 
 
-def test_preservation_incorrect_specs(preservation_bad_7209934):
-    
+def test_preservation_incorrect_specs(preservation_7209934):
     invalid_files = ["6895567.tif", "7210439.tif"]
-    
+
     validator_factory = validators.PreservationValidators()
     validator = validator_factory.technical_checker()
 
-    for root, dirs, files in preservation_bad_7209934:
+    for root, dirs, files in preservation_7209934:
         for file_ in files:
 
             file_name = os.path.join(root, file_)
