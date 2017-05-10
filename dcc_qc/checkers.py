83ced44d239ed3c9634c216bc074d592a6d12011
hborcher <hborcher@illinois.edu>
2017-05-10 16:39:45 -0500

    refactored Results from a namedtuple to class and wrote AccessCompletenessChecker implementation

--- dcc_qc/checkers.py
+++ dcc_qc/checkers.py
@@ -1,9 +1,21 @@
 import os
 import re
-from collections import namedtuple
 import abc
 
-Results = namedtuple("Results", ["valid", "errors"])
+
+class Results:
+    def __init__(self, valid, errors):
+        self._valid = valid
+        self._errors = errors
+
+    @property
+    def valid(self)->bool:
+        return self._valid
+
+    @property
+    def errors(self)->list:
+        return self._errors
+# Results = namedtuple("Results", ["valid", "errors"])
 
 
 class AbsValidator(metaclass=abc.ABCMeta):
@@ -56,9 +68,60 @@ class PresTechnicalChecker(AbsValidator):
 
 
 class AccessCompletenessChecker(AbsValidator):
-    # TODO: Create AccessCompletenessChecker() class
-    def check(self, file):
-        pass
+    def check(self, path: str):
+        """
+        Look for image there is an equal text file and vice-versa
+        Look for checksums.md5, marc.xml, and meta.yml files
+        Args:
+            path: 
+
+        Returns:
+
+        """
+        required_files = {"checksum.md5", "marc.xml", "meta.yml"}
+        valid_image_extensions = [".tif"]
+        valid_text_extensions = [".txt"]
+        errors = []
+        valid = True
+        image_files = set()
+        text_files = set()
+
+        # Sort the files into their own category
+        for root, dirs, files in os.walk(path):
+            for f in files:
+
+                # if the filename is the required files set, remove them
+                if f in required_files:
+                    required_files.remove(f)
+
+                basename, ext = os.path.splitext(f)
+                if ext in valid_image_extensions:
+                    image_files.add((root, f))
+                elif ext in valid_text_extensions:
+                    text_files.add((root, f))
+
+        # If there are any files still in the required_files set are missing.
+        for _file in required_files:
+            valid = False
+            errors.append("{} is missing {}".format(path, _file))
+
+        # check that for every .tif file there is a matching .txt
+        for img_path, img_file in image_files:
+            basename, ext = os.path.splitext(img_file)
+            required_text_file = basename + ".txt"
+            if (img_path, required_text_file) not in text_files:
+                valid = False
+                errors.append("{} is missing a matching {} file.".format(os.path.join(img_path, img_file), required_text_file))
+
+        # check that for every .txt file there is a matching .tif
+        for txt_path, txt_file in text_files:
+            basename, ext = os.path.splitext(txt_file)
+            required_tif_file = basename + ".tif"
+            if (txt_path, required_tif_file) not in image_files:
+                valid = False
+                errors.append("{} is missing a matching {}".format(os.path.join(txt_path, txt_file), required_tif_file))
+
+        return Results(valid=valid, errors=errors)
 
 
 class AccessNamingChecker(AbsValidator):
