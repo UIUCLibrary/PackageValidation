337ddeed7ba3983b7152906fbd6e6a5f2dbdaf54
hborcher <hborcher@illinois.edu>
2017-05-17 17:40:35 -0500

    TaskManager now implements a multableSequence interface

--- dcc_qc/task_manager.py
+++ dcc_qc/task_manager.py
@@ -1,63 +1,11 @@
-from collections import abc
-from enum import Enum
+import collections
 import typing
-from dcc_qc import checkers
+
 from dcc_qc import process
+from dcc_qc.tasks import Task
 
 
-class TaskStatus(Enum):
-    QUEUED = "queued"
-    WORKING = "working"
-    FAILED = "failed"
-    SUCCESS = "success"
-
-
-class Task:
-    """The Task class contains all the processes need to run by the TaskManager class"""
-
-    def __init__(self, name="Generic task"):
-        self._processes = []
-        self._name = name
-        self._results = []
-        self._errors = []
-        self.status = TaskStatus.QUEUED
-
-    def add_process(self, p):
-        self._processes.append(p)
-
-    def run(self):
-        self.status = TaskStatus.WORKING
-
-        for p in self._processes:
-            p.run()
-            for error in p.errors:
-                self._errors.append(error)
-
-            if isinstance(p, process.AbsProcessorResults):
-                self._results.append(p.result)
-
-        if self._errors:
-            self.status = TaskStatus.FAILED
-        else:
-            self.status = TaskStatus.SUCCESS
-
-    @property
-    def name(self):
-        """Short and human-readable result_type explaining what the task does."""
-        return self._name
-
-    @property
-    def results(self) -> typing.List[checkers.Results]:
-        """List of public results produced during the run() method, if any."""
-        return self._results
-
-    @property
-    def errors(self):
-        """List of any errors produced during the run() method, if any."""
-        return self._errors
-
-
-class TaskManager(abc.Collection):
+class TaskManager(collections.abc.MutableSequence):
     def __init__(self):
         self._processes = []
 
@@ -70,9 +18,21 @@ class TaskManager(abc.Collection):
     def __len__(self):
         return len(self._processes)
 
-    def __iter__(self) -> process.AbsProcess:
+    def __iter__(self) -> typing.Iterable[process.AbsProcess]:
         for p in self._processes:
             yield p
 
-    def push_task(self, task: Task):
+    def __getitem__(self, index):
+        return self._processes[index]
+
+    def __delitem__(self, index):
+        del self._processes[index]
+
+    def __setitem__(self, index, value):
+        self._processes[index] = value
+
+    def insert(self, index, value):
+        self._processes.insert(index, value)
+
+    def push(self, task: Task):
         self._processes.append(task)
