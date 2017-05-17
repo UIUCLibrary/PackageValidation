c898a80aea170b40e5fc601322ed9950ef873890
hborcher <hborcher@illinois.edu>
2017-05-17 17:41:30 -0500

    Added tests for the new state task

--- tests/test_taskManager.py
+++ tests/test_taskManager.py
@@ -1,10 +1,13 @@
 import pytest
+
+from dcc_qc import tasks
 from dcc_qc import task_manager
+from dcc_qc import task_states
 
 
 @pytest.fixture
-def my_task():
-    return task_manager.Task(name="dummy task")
+def task_fixture():
+    return tasks.Task(name="dummy task")
 
 
 @pytest.fixture
@@ -12,9 +15,31 @@ def my_task_manager():
     return task_manager.TaskManager()
 
 
-def test_my_task(my_task):
-    assert my_task.name == "dummy task"
-
-
 def test_manage_empty_size(my_task_manager):
     assert len(my_task_manager) == 0
+
+
+def test_task2_name(task_fixture):
+    assert task_fixture.name == "dummy task"
+
+
+def test_task2_empty(task_fixture):
+    assert len(task_fixture) == 0
+    assert task_fixture.status == task_states.TaskStatus.EMPTY
+
+
+def test_task2_add_to_empty(task_fixture):
+    task_fixture.add_process("DUMMY")
+    assert len(task_fixture) == 1
+    assert task_fixture.status == task_states.TaskStatus.QUEUED
+    task_fixture.add_process("DUMMY2")
+    assert len(task_fixture) == 2
+    assert task_fixture.status == task_states.TaskStatus.QUEUED
+
+
+def test_task2_reset_from_queued_state(task_fixture):
+    task_fixture.add_process("DUMMY")
+    task_fixture.add_process("DUMMY2")
+    task_fixture.reset()
+    assert len(task_fixture) == 0
+    assert task_fixture.status == task_states.TaskStatus.EMPTY
