24e0a8d7e4518d5cc8efef83f0da48d7d5bad3c0
hborcher <hborcher@illinois.edu>
2017-05-18 14:57:48 -0500

    added access folder check

--- dcc_qc/dcc_qc_runner.py
+++ dcc_qc/dcc_qc_runner.py
@@ -4,43 +4,58 @@ I'm using it to check that my validators work and my package API make
 sense when it is consumed by a script.
 """
 import os
+import typing
 
-import tasks
-from dcc_qc import task_manager
+from dcc_qc import task_manager, tasks, packages
 from dcc_qc import validation_processors
 
-TEST_PATH = "Z:/MedusaStaging/MappingHistory_Archives/20170323_MappingHistory"
-# TEST_PATH = "T:\HenryTest-PSR_2\DCC\Preservation_BAD"
+TEST_PATH = "T:/HenryTest-PSR_2/DCC\Package_GOOD/"
 
 
-# TEST_PATH = "T:\HenryTest-PSR_2\DCC\Preservation_GOOD"
-
-
-def get_package_folders(path):
+def get_package_folders(path)->typing.Generator[packages.abs_package.AbsPackage, None, None]:
     with os.scandir(path) as search_path:
         for item in search_path:
             if item.is_dir():
                 yield item.path
 
 
+def get_packages(path):
+    packages = []
+    return packages
+
+
 def main():
-    packages = get_package_folders(TEST_PATH)
+    # packages = get_packages(TEST_PATH)
+    hathi_packages_folders = get_package_folders(TEST_PATH)
+
     manager = task_manager.TaskManager()
 
     # Setup tasks
-    for path in packages:
-        my_task = tasks.Task(name=path)
+    for folder in hathi_packages_folders:
+        hathi_packages = packages.create_package("Hathi", folder)
+        for hathi_package in hathi_packages:
+            task_name = hathi_package.directories["preservation"].split(os.path.sep)[-1]
 
-        preservation_package_test = validation_processors.PackagePreservationComplete()
-        preservation_package_test.setup()
-        preservation_package_test.set_input(path)
-        my_task.add_process(preservation_package_test)
+            my_task = tasks.Task(description="Validating {} in {}".format(task_name, hathi_package.root))
 
-        manager.push(my_task)
+            # Preservation Folder
+            preservation_folder_completeness_test = validation_processors.PackagePreservationComplete()
+            preservation_folder_completeness_test.setup()
+            preservation_folder_completeness_test.set_input(hathi_package.directories["preservation"])
+            my_task.add_process(preservation_folder_completeness_test)
+
+            # Access folder
+            access_folder_completeness_test = validation_processors.PackageAccessComplete()
+            access_folder_completeness_test.setup()
+            access_folder_completeness_test.set_input(hathi_package.directories['access'])
+            my_task.add_process(access_folder_completeness_test)
+
+
+            manager.push(my_task)
 
     # run tasks
     for i, task in enumerate(manager):
-        print("({}/{}) Checking: {}".format(i + 1, len(manager), task.name))
+        print("({}/{}): {}".format(i + 1, len(manager), task.name))
         task.run()
         results = task.results
         if task.errors:
