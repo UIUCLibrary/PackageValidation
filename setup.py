ca2150ff42d791a5582e5446cbd31c3cfb254a04
hborcher <hborcher@illinois.edu>
2017-05-22 14:40:57 -0500

    added entry_points.

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='dcc_hathi_qc',
+    name='dcc_qc',
     version='0.0.1a',
     packages=['dcc_qc'],
     test_suite="tests",
     tests_require=['pytest'],
-    url='',
+    url='https://github.com/UIUCLibrary/HathiTrustPackaging',
+    entry_points={"console_scripts": ["qc = dcc_qc.cli:main"]},
     license='',
     author='Henry Borchers',
     author_email='hborcher@illinois.edu',
-    description=''
+    description='This package is for performing automated quality control tests on file packages'
 )
