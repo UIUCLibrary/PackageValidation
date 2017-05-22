b7818944975822fa0bc0db3e66a6a3aac8e74d26
hborcher <hborcher@illinois.edu>
2017-05-22 16:06:38 -0500

    Simplied create_pairs local function of the load() method in the HathiPackage class.

--- dcc_qc/packages/hathi.py
+++ dcc_qc/packages/hathi.py
@@ -1,15 +1,14 @@
-from dcc_qc.packages import abs_package
+from dcc_qc.packages.abs_package import AbsPackage, PackageItem
+from dcc_qc import packages
 import typing
 import os
-import itertools
 
 
-class HathiPackage(abs_package.AbsPackage):
+class HathiPackage(AbsPackage):
     @property
-    def items(self) -> typing.Generator[abs_package.PackageItem, None, None]:
+    def items(self) -> typing.Generator[PackageItem, None, None]:
         for i in self._items:
             yield i
-
 
     def load(self):
         def get_package_paths(path) -> typing.Tuple[str, str]:
@@ -30,34 +29,24 @@ class HathiPackage(abs_package.AbsPackage):
                 raise Exception("Error: Found multiple access or preservation folders in {}".format(path))
 
             if len(found_access) == 0 or len(found_preservation) == 0:
-                raise FileNotFoundError("Error: Missing access or preservation folders in {}".format(path))
-            raise Exception("Error: Unable to find access and preservation folders in {}".format(path))
+                raise packages.PackagePartMissing("Error: Missing access and/or preservation folders in {}".format(path))
+            raise packages.PackageError("Error: Unable to find access and preservation folders in {}".format(path))
 
         def create_pairs(access_path, preservation_path) -> typing.Generator[typing.Tuple[str, str], None, None]:
-            access_folders = [it.path for it in os.scandir(access_path)]
-            preservation_folders = [it.path for it in os.scandir(preservation_path)]
+            access_folders = sorted(os.listdir(access_path))
+            preservation_folders = sorted(os.listdir(preservation_path))
+
             assert len(access_folders) == len(preservation_folders)
-            combined = sorted(access_folders + preservation_folders, key=lambda it: str(it.split(os.path.sep)[-1]))
-            groups = itertools.groupby(combined, key=lambda it: str(it.split(os.path.sep)[-1]))
-            for group, items in groups:
-                new_access = None
-                new_preservation = None
-                for i in items:
-                    if i.split(os.path.sep)[-2] == "preservation":
-                        new_preservation = i
-                    elif i.split(os.path.sep)[-2] == "access":
-                        new_access = i
-                if new_access is not None and new_preservation is not None:
-                    yield new_access, new_preservation
-                else:
-                    raise Exception("Failed to match preservation folder with an access folder")
+            for pair in zip(access_folders, preservation_folders):
+                assert pair[0] == pair[1]
+                yield os.path.join(access_path, pair[0]), os.path.join(preservation_path, pair[1])
 
         access_path, preservation_path = get_package_paths(self.root_path)
         assert access_path.replace("access", "") == preservation_path.replace("preservation", "")
 
         for access, preservation in create_pairs(access_path, preservation_path):
             self._items.append(
-                abs_package.PackageItem(
+                PackageItem(
                     root=self.root_path,
                     directories={
                         "access": access,
