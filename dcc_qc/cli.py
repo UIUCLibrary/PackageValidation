16758085ff5bc6b9510eec8e1ddd86232aee2041
hborcher <hborcher@illinois.edu>
2017-05-22 13:35:56 -0500

    refactored to use HathiQCRunner class

--- dcc_qc/cli.py
+++ dcc_qc/cli.py
@@ -22,22 +22,26 @@ def main():
     if arg_errors:
         for er in arg_errors:
             print(er, file=sys.stderr)
-    errors = hathi_qc_runner.run(args.path)
+    runner = hathi_qc_runner.HathiQCRunner(args.path)
+    runner.run()
+
 
     # Summary
     print("\n")
-    print("================")
+    print("=====================")
     print("Summary:")
-    print("================")
-    print("Errors found: {}".format(len(errors)))
-    print("================")
-    if errors:
-        print("Errors:")
-        for error in errors:
-            print(error)
+    print("=====================")
+    print("Errors found: {}".format(len(runner.errors)))
+    print("=====================")
+    if not runner.valid:
+        print("Validation Failed")
+        if runner.errors:
+            print("Errors:")
+            for error in runner.errors:
+                print(error)
     else:
         print("All tests PASSED")
-    print("================")
+    print("=====================")
 
 
 if __name__ == '__main__':
