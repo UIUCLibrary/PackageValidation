bc12ba1637f45fad485bc88f823a6bd910ce0f71
hborcher <hborcher@illinois.edu>
2017-05-26 11:46:11 -0500

    added pytest

--- cx_setup.py
+++ cx_setup.py
@@ -1,6 +1,9 @@
 from cx_Freeze import setup, Executable
 import platform
-
+import pytest
+includes = [
+    'pkg_resources'
+] + pytest.freeze_includes()
 setup(
     name='dcc_qc',
     version='0.0.1',
@@ -9,6 +12,7 @@ setup(
         'dcc_qc.packages',
         'dcc_qc.task_states',
         'dcc_qc.validators',
+        'tests'
     ],
     url='https://github.com/UIUCLibrary/HathiTrustPackaging',
     description='This package is for performing automated quality control tests on file packages',
@@ -17,4 +21,6 @@ setup(
                    targetName=("qcpkg.exe" if platform.system() == "Windows" else "qcpkg"))
 
     },
+    options={"build_exe": {'includes': includes}},
+
 )
