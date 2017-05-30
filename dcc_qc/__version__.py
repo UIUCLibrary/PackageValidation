0188024b2ec33346d681ee2c5104af613cad00b3
hborcher <hborcher@illinois.edu>
2017-05-30 16:49:45 -0500

    moved metadata information in __version__.py

---
+++ dcc_qc/__version__.py
@@ -0,0 +1,5 @@
+__title__ = 'dcc_qc'
+__version__ = '0.0.1'
+__author__ = "Henry Borchers"
+__author_email__ = 'hborcher@illinois.edu'
+__description__ = 'This package is for performing automated quality control tests on file packages'
