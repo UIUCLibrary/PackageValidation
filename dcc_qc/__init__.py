c9982b500fde2439a0c53b9515e63cc82d443bd3
hborcher <hborcher@illinois.edu>
2017-05-30 16:52:19 -0500

    moved metadata information in the package

--- dcc_qc/__init__.py
+++ dcc_qc/__init__.py
@@ -1,1 +1,1 @@
-from .__version__ import __title__, __version__, __author__, __author_email__, __description__
+from .__version__ import __title__, __version__, __author__, __author_email__, __description__, __url__
--- dcc_qc/__version__.py
+++ dcc_qc/__version__.py
@@ -3,3 +3,4 @@ __version__ = '0.0.1'
 __author__ = "Henry Borchers"
 __author_email__ = 'hborcher@illinois.edu'
 __description__ = 'This package is for performing automated quality control tests on file packages'
+__url__ = 'https://github.com/UIUCLibrary/HathiTrustPackaging'
