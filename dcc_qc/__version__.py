1f11f5204faa9ebbc769f3981f7b25cf6f5dd0dc
hborcher <hborcher@illinois.edu>
2017-06-13 11:16:52 -0500

    Added FULL_NAME

--- dcc_qc/__version__.py
+++ dcc_qc/__version__.py
@@ -1,6 +1,8 @@
 __title__ = 'dcc_qc'
 __version__ = '0.0.2'
-__author__ = "Henry Borchers"
+__author__ = "University of Illinois at Urbana Champaign"
+__credits__ = "Henry Borchers"
 __author_email__ = 'hborcher@illinois.edu'
 __description__ = 'This package is for performing automated quality control tests on file packages'
 __url__ = 'https://github.com/UIUCLibrary/HathiTrustPackaging'
+FULL_NAME = "DCC Package QC"
