2afd6255e682e1a6b147d0e63dbf505468ce45e2
hborcher <hborcher@illinois.edu>
2017-06-14 11:03:11 -0500

    added six

--- cx_setup.py
+++ cx_setup.py
@@ -3,7 +3,50 @@ import platform
 import pytest
 import dcc_qc
 
-includes = ['pkg_resources'] + pytest.freeze_includes()
+includes = ['pkg_resources', 'six'] + pytest.freeze_includes()
+
+
+def create_msi_tablename(python_name, fullname):
+    shortname = python_name[:6].replace("_", "").upper()
+    longname = fullname
+    return "{}|{}".format(shortname, longname)
+
+
+EXECUTABLE_NAME = "qcpkg"
+
+directory_table = [
+    (
+        "ProgramMenuFolder",  # Directory
+        "TARGETDIR",  # Directory_parent
+        "PMenu|Programs",  # DefaultDir
+    ),
+    (
+        "PMenu",  # Directory
+        "ProgramMenuFolder",  # Directory_parent
+        create_msi_tablename(dcc_qc.__title__, dcc_qc.FULL_TITLE)
+    ),
+]
+shortcut_table = [
+    (
+        "startmenuShortcutDoc",  # Shortcut
+        "PMenu",  # Directory_
+        "{} Documentation".format(create_msi_tablename(dcc_qc.__title__, dcc_qc.FULL_TITLE)),
+        "TARGETDIR",  # Component_
+        "[TARGETDIR]documentation.url",  # Target
+        None,  # Arguments
+        None,  # Description
+        None,  # Hotkey
+        None,  # Icon
+        None,  # IconIndex
+        None,  # ShowCmd
+        'TARGETDIR'  # WkDir
+    ),
+]
+
+INCLUDE_FILES = [
+    "documentation.url"
+]
+
 setup(
     name=dcc_qc.__title__,
     version=dcc_qc.__version__,
@@ -18,13 +61,21 @@ setup(
     description=dcc_qc.__description__,
     executables={
         Executable("dcc_qc/cli.py",
-                   targetName=("qcpkg.exe" if platform.system() == "Windows" else "qcpkg"))
+                   targetName=("{}.exe".format(EXECUTABLE_NAME) if platform.system() == "Windows" else EXECUTABLE_NAME))
 
     },
-    options={"build_exe": {
-                'includes': includes,
-                "include_msvcr": True
+    options={
+        "build_exe": {
+            'includes': includes,
+            "include_msvcr": True
+        },
+        "bdist_msi": {
+            "data": {
+                "Shortcut": shortcut_table,
+                "Directory": directory_table
             }
+        }
+
     },
 
 )
