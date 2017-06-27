d6af991e994fb0a11fb69b0206e0d6647a67a2ad
hborcher <hborcher@illinois.edu>
2017-06-27 16:22:33 -0500

    updated name

--- dcc_qc/checkers/utils.py
+++ dcc_qc/checkers/utils.py
@@ -1,6 +1,7 @@
-from .validator_factory import ValidatorFactory
+from . import check_suite
 
-def get_validator(name):
-    factory = ValidatorFactory()
+
+def get_check_suite(name)->check_suite.AbsCheckSuite:
+    factory = check_suite.CheckSuiteFactory()
     new_validator = factory.create_instance(name)
     return new_validator
