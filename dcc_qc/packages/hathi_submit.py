5e087488445aba688405c577a8c36b13f874947a
hborcher <hborcher@illinois.edu>
2017-06-27 16:20:28 -0500

    fixed bug with the root removed get_package_level_files()

--- dcc_qc/packages/hathi_submit.py
+++ dcc_qc/packages/hathi_submit.py
@@ -11,14 +11,10 @@ class HathiSubmitPackage(AbsPackage):
     def get_packages(path):
         for package_path in filter(lambda x: x.is_dir(),os.scandir(path)):
             yield PackageItem(
-                root=path,
+                root=package_path.path,
                 identifier=package_path.name,
                 directories={
                     "access": package_path.path,
                 }
             )
 
-
-    def get_package_level_files(self) -> typing.Iterable[PackageItem]:
-        #TODO: get_package_level_files
-        return super().get_package_level_files()
