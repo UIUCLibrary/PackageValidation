8e31ed65e7f552ef4da0bc1b838951ea3d2719e3
Henry Borchers <henryborchers@yahoo.com>
2018-08-13 10:29:38 -0500

    sphinx config includes doc test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,8 +32,13 @@ import dcc_qc
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.githubpages', 'sphinx.ext.napoleon']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.coverage',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon'
+]
 
 napoleon_google_docstring = True
 napoleon_numpy_docstring = True
