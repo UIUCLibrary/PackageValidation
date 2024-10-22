8fa5ab8b9502416c475d7a4a8d0d547589e42fcb
Henry Borchers <henryborchers@yahoo.com>
2024-10-22 11:38:43 -0500

    build: move project metadata to pyproject.toml

--- dcc_qc/__init__.py
+++
@@ -1,2 +0,0 @@
-from .__version__ import __title__, __version__, __author__, __author_email__
-from .__version__ import __description__, __url__, FULL_TITLE
--- dcc_qc/__version__.py
+++
@@ -1,8 +0,0 @@
-__title__ = 'dcc_qc'
-__version__ = '0.1.3.dev0'
-__author__ = "University of Illinois at Urbana Champaign"
-__credits__ = "Henry Borchers"
-__author_email__ = 'hborcher@illinois.edu'
-__description__ = 'This package is for performing automated quality control tests on file packages'
-__url__ = 'https://github.com/UIUCLibrary/HathiTrustPackaging'
-FULL_TITLE = "DCC Package QC"
