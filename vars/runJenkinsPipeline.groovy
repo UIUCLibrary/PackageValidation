eb3d15885e1b08643d8e7584e63b187d38d7cf5b
Henry Borchers <henryborchers@yahoo.com>
2026-03-19 12:29:53 -0500

    ci: support python 3.14

--- vars/runJenkinsPipeline.groovy
+++ vars/runJenkinsPipeline.groovy
@@ -461,7 +461,7 @@ def call(){
                                 axes: [
                                     [
                                         name: 'PYTHON_VERSION',
-                                        values: ['3.10', '3.11', '3.12','3.13']
+                                        values: ['3.10', '3.11', '3.12', '3.13', '3.14']
                                     ],
                                     [
                                         name: 'OS',
